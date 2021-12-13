421e622c7a0846bc0856574aff0ef0622eae0ff6
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2021-12-13 15:25:15 -0300

    como selecionar valores para inputs do tipo radio

---
+++ Jquery/script.js
@@ -0,0 +1,11 @@
+ 
+ 
+ 
+    $(document).ready( () => {
+        $('img').attr('border', 5) 
+        $('img').attr('width', 800)
+    
+        let x = $('div:first').html
+        console.log(x)
+    })
+
